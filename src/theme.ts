bf2476b869b9ae2f1cbacd926a6eeba7745f0e25
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-12-09 01:12:55 +0900

    #7.0
    +"1564123" 이렇게하면 1564123은 number 타입이 된다.

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "black",
+  accentColor: "9c88ff",
+};
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "white",
+  accentColor: "#9c88ff",
+};

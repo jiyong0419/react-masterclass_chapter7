d823312c3dbc2f6a1a7bff4b524769ec6bef0425
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-12-10 16:57:57 +0900

    #7.5
    onDragEnd의 args
    Array.spilice( )

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { toDoState } from "./atoms";
 
 const Wrapper = styled.div`
   display: flex;
@@ -31,9 +38,11 @@ const Card = styled.div`
   border-radius: 5px;
 `;
 
-const toDos = ["a", "b", "c", "d", "e", "f"];
 function App() {
-  const onDragEnd = () => {};
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDragEnd = ({ destination, source }: DropResult) => {
+    console.log(destination, source);
+  };
   return (
     <Wrapper>
       <Boards>
@@ -43,7 +52,7 @@ function App() {
               {(magic) => (
                 <Board ref={magic.innerRef} {...magic.droppableProps}>
                   {toDos.map((toDo, index) => (
-                    <Draggable draggableId={toDo} index={index}>
+                    <Draggable key={index} draggableId={toDo} index={index}>
                       {(magic) => (
                         <Card
                           ref={magic.innerRef}
